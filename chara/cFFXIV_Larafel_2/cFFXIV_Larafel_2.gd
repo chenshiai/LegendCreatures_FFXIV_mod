fbf88c3aa75f1600a1a45109971a61dce263025c
chenshiai <chenshiai@github.com>
2020-05-19 12:03:36 +0800

    fix: 黑魔代码优化

--- chara/cFFXIV_Larafel_2/cFFXIV_Larafel_2.gd
+++ chara/cFFXIV_Larafel_2/cFFXIV_Larafel_2.gd
@@ -9,10 +9,10 @@ func _extInit():
 	attCoe.atkRan = 3
 	attCoe.maxHp = 3
 	attCoe.atk = 1
-	attCoe.mgiAtk = 2
+	attCoe.mgiAtk = 7
 	attCoe.def = 3
 	attCoe.mgiDef = 3
-	lv = 1
+	lv = 2
 	evos = []
 	atkEff = "atk_dang"
 	addCdSkill("skill_FireIII", 25)#添加cd技能
@@ -22,8 +22,14 @@ func _extInit():
 [爆炎]：复唱时间25s，对目标发动火属性魔法攻击，威力：240
 [玄冰]：复唱时间6s，对目标发动冰属性范围魔法攻击，威力：100""")
 
-var fire = 0
-var freeze = 0
+var fire = 0 # 星极火当前阶段
+var freeze = 0 # 灵极冰当前层数
+const FIRE_PW = 0.30 # 星极火倍率
+const FREEZE_MAX = 3 # 灵极冰最大值
+const FIRE_CD = 21 # 每次减火系魔法的cd值
+const FIREIII_PW = 2.40 # 爆炎威力
+const FREEZE_PW = 1.00 # 玄冰威力
+
 #进入战斗初始化，事件连接在这里初始化
 func _connect():
 	._connect() #保留继承的处理
@@ -36,33 +42,40 @@ func _onBattleStart():
 func _castCdSkill(id):
 	._castCdSkill(id)
 	if id == "skill_FireIII" && aiCha != null:
-		var eff:Eff = newEff("sk_yunShi")
-		eff.position = aiCha.position
-		eff.scale *= 1.2
-		yield(reTimer(0.45),"timeout")
-		hurtChara(aiCha, att.mgiAtk * (2.40 + 0.3 * fire), HurtType.MGI)
-		fire += 1
-		if freeze > 0:
-			freeze -= 1
-			var sk = getSkill("skill_FireIII")
-			var fz = getSkill("skill_Freeze")
-			sk.nowTime = 21
-			fz.nowTime = 0
-
+		fireIII(aiCha)
 	if id == "skill_Freeze" && aiCha != null:
-		fire = 0
-		freeze = 3
-		var sk = getSkill("skill_FireIII")
-		sk.nowTime = 21
-		
-		var cell = aiCha.cell
-		var chas = getCellChas(cell, 1)
+		freezeIII(aiCha)
+	
+func fireIII(aiCha):
+	var eff:Eff = newEff("sk_yunShi")
+	eff.position = aiCha.position
+	eff.scale *= 1.2
+	yield(reTimer(0.45),"timeout")
 
-		for i in range(4):
-			var eff:Eff = newEff("sk_bingYu")
-			eff.position = aiCha.position
-		
-		yield(reTimer(0.5),"timeout")
-		for i in chas:
-			if i != null: 
-				hurtChara(i, att.mgiAtk * 1.00, HurtType.MGI)
+	hurtChara(aiCha, att.mgiAtk * (FIREIII_PW + FIRE_PW * fire), HurtType.MGI)
+
+	fire += 1
+	if freeze > 0:
+		freeze -= 1
+		var sk = getSkill("skill_FireIII")
+		var fz = getSkill("skill_Freeze")
+		sk.nowTime = FIRE_CD
+		fz.nowTime = 0
+
+func freezeIII(aiCha):
+	fire = 0
+	freeze = FREEZE_MAX
+	var sk = getSkill("skill_FireIII")
+	sk.nowTime = FIRE_CD
+	
+	var cell = aiCha.cell
+	var chas = getCellChas(cell, 1)
+
+	for i in range(4):
+		var eff:Eff = newEff("sk_bingYu")
+		eff.position = aiCha.position
+	
+	yield(reTimer(0.5),"timeout")
+	for i in chas:
+		if i != null: 
+			hurtChara(i, att.mgiAtk * FREEZE_PW, HurtType.MGI)
