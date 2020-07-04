468c8327f0ddbd5872b661676cd650e01c9bc702
chenshiai <chenshiai@github.com>
2020-07-04 16:27:11 +0800

    fix: 无瑕灵君血量提升

--- BossInnocence/cFFXIVBossInnocence_Hide/BossChara.gd
+++ BossInnocence/cFFXIVBossInnocence_Hide/BossChara.gd
@@ -107,7 +107,7 @@ func selfAdaption():
 		E_spd = allAtt.spd
 		E_num = allAtt.num
 		E_lv = allAtt.lv
-		attInfo.maxHp = (E_atk + E_mgiAtk) / E_num * layer * 8
+		attInfo.maxHp = (E_atk + E_mgiAtk) / E_num * layer * 10
 		attInfo.atk = (E_def + E_maxHp / 8) / E_num + layer * 3
 		attInfo.mgiAtk = (E_mgiDef + E_maxHp / 8) / E_num + layer * 3
 		attInfo.def = (E_atk + layer * 2) / E_num
