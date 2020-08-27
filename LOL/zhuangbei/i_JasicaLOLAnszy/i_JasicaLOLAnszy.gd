bfa85369151f6a746bd368f2e827f2676fca25b6
chenshiai <chenshiai@github.com>
2020-08-28 00:31:53 +0800

    fix: wenb

--- LOL/zhuangbei/i_JasicaLOLAnszy/i_JasicaLOLAnszy.gd
+++ LOL/zhuangbei/i_JasicaLOLAnszy/i_JasicaLOLAnszy.gd
@@ -6,6 +6,7 @@ func _init():
 	name = "纳什之牙"
 	att.spd = 0.5
 	att.mgiAtk = 80
+	att.cd = 0.2
 	info = TEXT.format("""{c_base}{c_skill}唯一被动：{/c}+20%冷却缩减
 {c_skill}唯一被动：{/c}普通攻击会造成{c_mgi}25(+15% x 法强){/c}的额外魔法伤害{/c}""")
 	
